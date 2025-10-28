6551e460d7e1bb44669bd41ee72af0ee05d03af7
vladpodilnyk <vlad.podilnyk@gmail.com>
2025-10-28 23:15:29 +0100

    wip: connect UI and search API.

--- workers/ui/src/worker/index.ts
+++ workers/ui/src/worker/index.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
+import type SearchService from "../../../search-service/src/index";
 
-const app = new Hono();
+type Bindings = {
+  SEARCH_SERVICE: Service<SearchService>;
+};
 
-app.get("/api/", (c) => c.json({ name: "Hello world!" }));
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/api/", async (c) => {
+  const res = await c.env.SEARCH_SERVICE.search();
+  return c.json({ name: res });
+});
 
 export default app;
